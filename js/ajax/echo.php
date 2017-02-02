3d44ecbbe5bfc2a282181b46e2130ef23fba2f80
eduvallejo <vallejoprietoeduardo@gmail.com>
2017-02-03 00:14:48 +0100

    los php para conseguir la ruta y luego pasar el pattern. he camdiado la rut escrita a lo duro por el dirname(__FILE__) asi no habre de cambiar la ruta cuando pase de un pc a otro

--- js/ajax/echo.php
+++ js/ajax/echo.php
@@ -1,14 +1,18 @@
 <?php 
 $targetName = $_GET["name"];
-$path = realpath('/var/www/conway/structures/');
+// $path = realpath('/var/www/html/conway/structures/');
+// echo('\$path: '. $path . '<br>');
+$path = dirname(__FILE__); 
+$path = str_replace("js/ajax", "structures", $path);
+// echo 'path: ' . $path. '<br>';
+
+
 
 $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
 foreach($objects as $name => $object){
     if (basename($name) == $targetName) {
-    	// echo("\$name : " . $name . "<br>");
     	
-    	$result = str_replace("/var/www/conway/structures/", "../../structures/", $name);
-    	// echo $result;
+    	$result = str_replace($path , "../../structures", $name);
     }
 }
 
@@ -16,60 +20,36 @@ $file = fopen($result , "r");
 if (!$file) {
 	echo 'Could not open: ' . $result;
 }
-$pattern = [];//haremos un array con 3, el 0 sera x , el 1 la y y el 3 el rle
 for ($i=0; $i < 3; $i++) { 
 	$pattern[$i] = "";
 }
-// $file = fopen($path, "r");
 while(!feof($file)){
-	//get each time one line 
 	$linea =(fgets($file));
 	if ($linea[0] != "#" && $linea[0] != "x" && $linea[0] != "y") {
 			# code...
-		// we store the position into the string of our token: parentesis, comas, etc..
 		$pos = strpos($linea, "\n");
-		//filtramos solo los q llevan una coma
 		if ($pos !== false) {
 		
 			$sublinea = substr($linea, 0, $pos); 
 			$pattern[2] = $pattern[2] . $sublinea;
-			// echo $pattern[2];
-			// echo '--- <br>';
 		}
 	}else if($linea[0] == "x" || $linea[0] == "y"){
 		$posEqualx = strpos($linea, "=");
-		// echo $posEqualx . "<br>";
 		$posCommaX = strpos($linea, ",");
-		// $posY = strpos($linea, "y");
-		// echo $posY . "<br>";
 		$pattern[0] = substr($linea, $posEqualx + 2, ($posCommaX - $posEqualx)-2);
-		// echo "X = " . $pattern[0]  . "<br>";
 
 		$posEqualY = strpos($linea, "=",  $posCommaX);
-		// echo $posEqualY . "<br>";
 		$posCommaY = strpos($linea, ",", $posEqualY);
-		// echo $posCommaY . "<br>";
 		$posY = strpos($linea, "y");
-		// echo $posY . "<br>";
 		$pattern[1] = substr($linea, $posEqualY + 2, ($posCommaY - $posEqualY)-2);
-		// echo "Y = " . $pattern[1] . "<br>";
 	}
 }
-// echo $pattern;
 
-// foreach ($pattern as $key => $value) {
-// 	echo  $value ;
-// }
 
-// $ar = array($pattern[0], $pattern[1], $pattern[2]);
-// echo json_encode($ar);
 
 echo json_encode($pattern);
 
-// echo $pattern[0];	
-// echo $pattern[1];	
-// echo $pattern[2];	
-fclose($file);
+// fclose($file);
 
 
 
--- js/select/getNamesPatterns.php
+++ js/select/getNamesPatterns.php
@@ -1,18 +1,18 @@
 <?php 
 // $targetName = $_GET["name"];
 
-$path = realpath('/var/www/conway/structures/');
+// $path = realpath('/var/www/html/conway/structuresDebug/');
+$path = dirname(__FILE__); //echoes path: /var/www/html/conway/js/select
+$path = str_replace("js/select", "structures", $path);
+
+// ../../structures/
 $nameArray = array();
 $objects = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path), RecursiveIteratorIterator::SELF_FIRST);
 foreach($objects as $name => $object){
 	// echo 
 	if (!strpos($name, "~")) {
 		if (!is_dir($name)) {
-	    	
 			$name = basename($name);
-			// $needles = array(".rle", ".lif", ".txt", ".zip");
-			// $name = str_replace($needles, "", $name);
-			// echo $name.'<br>';
 			array_push($nameArray, $name);
 		}    
 	}
